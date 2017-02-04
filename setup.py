87a4c28087dbfd049e7ec57dc435f3c0f5ec9d71
Daniele Maccioni <gendoikari@develer.com>
2017-02-04 01:30:11 +0100

    Fixed broken release.

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from distutils.core import setup
 
 setup(
     name = 'pymicro',
-    packages = ['pymicro'],
-    version = '0.0.1',
+    packages = ['pymicro', 'pymicro.protocols'],
+    include_package_data=True,
+    version = '0.0.2',
     description = 'An easy to use python3 package to create and use microservices.',
     author = 'Daniele Maccioni',
     author_email = 'komradstudios@gmail.com',
     url = 'https://github.com/GendoIkari/pymicro',
-    download_url = 'https://github.com/GendoIkari/pymicro/tarball/v0.0.1',
+    download_url = 'https://github.com/GendoIkari/pymicro/tarball/v0.0.2',
     keywords = ['microservices', 'rabbitmq', 'service'],
     classifiers = [],
     install_requires=['flask', 'requests', 'pika'],
